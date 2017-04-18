5f95d615b04c450b40522cf3c186fdb7c6b82f8d
root <monty.joypur@gmail.com>
2017-04-17 23:45:14 -0700

    Adding First Driver Project

---
+++ MyFirstDriverProj/.tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/monojit/WindowsHostShared/Shared_Folder/Experiment/April/MyFirstDriverProj/hello.ko
+/home/monojit/WindowsHostShared/Shared_Folder/Experiment/April/MyFirstDriverProj/hello.o
---
+++ MyFirstDriverProj/hello.c
@@ -0,0 +1,44 @@
+#include <linux/init.h>             // Macros used to mark up functions e.g., __init __exit
+#include <linux/module.h>           // Core header for loading LKMs into the kernel
+#include <linux/kernel.h>           // Contains types, macros, functions for the kernel
+ 
+MODULE_LICENSE("GPL");              ///< The license type -- this affects runtime behavior
+MODULE_AUTHOR("Monojit Chatterjee");      ///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("A simple Linux driver for the Start .");  ///< The description -- see modinfo
+MODULE_VERSION("1.2");              ///< The version of the module
+ 
+static char *name = "world";        ///< An example LKM argument -- default value is "world"
+
+static int value = 12;
+module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
+
+module_param(value, int , S_IRUGO);
+
+MODULE_PARM_DESC(name, "The name to display in /var/log/kern.log");  ///< parameter description
+ 
+/** @brief The LKM initialization function
+ *  The static keyword restricts the visibility of the function to within this C file. The __init
+ *  macro means that for a built-in driver (not a LKM) the function is only used at initialization
+ *  time and that it can be discarded and its memory freed up after that point.
+ *  @return returns 0 if successful
+ */
+static int __init helloBBB_init(void){
+   printk(KERN_INFO "[Hello World] Hello %s from the Linux Kernel! and the value is %d\n", name, value);
+   printk(KERN_ALERT "[Hello World] Sending Alert signal to careless Kernel \n");
+   return 0;
+}
+ 
+/** @brief The LKM cleanup function
+ *  Similar to the initialization function, it is static. The __exit macro notifies that if this
+ *  code is used for a built-in driver (not a LKM) that this function is not required.
+ */
+static void __exit helloBBB_exit(void){
+   printk(KERN_INFO "EBB: Goodbye %s from the Linux Kernel! and the value is: %d\n", name, value);
+}
+ 
+/** @brief A module must use the module_init() module_exit() macros from linux/init.h, which
+ *  identify the initialization function at insertion time and the cleanup function (as
+ *  listed above)
+ */
+module_init(helloBBB_init);
+module_exit(helloBBB_exit);
