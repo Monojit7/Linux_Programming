3ab68bbb90afb511334582ba859d54c0f109f740
root <monty.joypur@gmail.com>
2017-04-20 04:22:26 -0700

    adding folder structure cmake

---
+++ CMakeProject/CMake_Project_with_Directories/build/CMakeFiles/Hello_World.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/monojit/WindowsHostShared/Shared_Folder/Experiment/April/gitRepos/Linux_Programming/CMakeProject/CMake_Project_with_Directories/src/main.cpp" "/home/monojit/WindowsHostShared/Shared_Folder/Experiment/April/gitRepos/Linux_Programming/CMakeProject/CMake_Project_with_Directories/build/CMakeFiles/Hello_World.dir/src/main.cpp.o"
+  "/home/monojit/WindowsHostShared/Shared_Folder/Experiment/April/gitRepos/Linux_Programming/CMakeProject/CMake_Project_with_Directories/src/student.cpp" "/home/monojit/WindowsHostShared/Shared_Folder/Experiment/April/gitRepos/Linux_Programming/CMakeProject/CMake_Project_with_Directories/build/CMakeFiles/Hello_World.dir/src/student.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  ".././include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
