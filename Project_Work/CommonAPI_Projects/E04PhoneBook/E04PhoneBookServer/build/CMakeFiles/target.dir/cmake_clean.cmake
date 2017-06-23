69f0b3035fbd1684bae5c05bf3d74bb155c0be6c
Monojit Chatterjee <you@example.com>
2017-06-23 07:12:04 -0700

    Added README

--- Project_Work/CommonAPI_Projects/E04PhoneBook/E04PhoneBookServer/build/CMakeFiles/target.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/target"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Project_Work/CommonAPI_Projects/E04PhoneBook/E04PhoneBookServer/build/src-gen/CMakeFiles/E04PhoneBookServer_E04PhoneBookServer_types_static_stub.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libE04PhoneBookServer_E04PhoneBookServer_types_static_stub.a"
-)
