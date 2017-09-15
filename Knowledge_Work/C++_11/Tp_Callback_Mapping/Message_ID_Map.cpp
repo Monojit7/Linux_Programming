e2150580e298ee3619ba51dc5e71918140039987
Chatterjee, Monojit <Monojit.Chatterjee@harman.com>
2017-09-15 04:21:22 -0700

    CallBack Mapping

---
+++ Knowledge_Work/C++_11/Tp_Callback_Mapping/Message_ID_Map.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <map>
+#include <memory>
+#include <functional>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+using namespace std;
+using namespace placeholders;
+typedef shared_ptr < vector < string > >   tpdata ;
+    
+    // handler function
+typedef function < bool ( tpdata ) > handlerType ;
+
+map < int, handlerType >    messageIdHandlerMap_ ;
+
+class DataHandler
+{
+
+public:
+
+DataHandler()
+{
+
+  this->initDataHandler();
+}
+
+void initDataHandler();
+bool nameReceived(tpdata data );
+
+void func(vector<string> namelist, int value);
+
+};
+
+void DataHandler::func(vector <string> namelist, int value)
+{
+
+  cout << "   " << __FUNCTION__ << "   " << __LINE__ << endl;
+  auto data = make_shared < vector < string > > ();
+  *data = namelist; 
+  messageIdHandlerMap_[value] (data);
+
+}
+
+bool DataHandler::nameReceived(tpdata data)
+{
+
+cout << "   " << __FUNCTION__ << "   " << __LINE__ << endl;
+
+for ( auto i : (*data))
+cout << "data value is:  " << i << endl;
+
+
+}
+
+
+void DataHandler::initDataHandler()
+{
+
+cout << "   " << __FUNCTION__ << "   " << __LINE__ << endl;
+
+    messageIdHandlerMap_ = 
+    {
+        /* trackNameSt */       {   0x46,   bind ( &DataHandler::nameReceived, this,   _1 )   } 
+    };
+
+}
+
+int main()
+{
+
+  DataHandler obj;
+  vector <string> namelist = {"Monojit", "Chatterjee", "GarhJoypur", "Purulia"};
+  obj.func(namelist, 0x46 );
+ 
+  return 0;
+
+}
