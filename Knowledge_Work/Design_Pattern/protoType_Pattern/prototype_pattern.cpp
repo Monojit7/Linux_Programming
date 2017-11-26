05b41326c37af996e3d96826a3814d074488c4d1
Chatterjee, Monojit <Monojit.Chatterjee@harman.com>
2017-11-26 07:24:50 -0800

    [ Design Pattern ] protoType pattern

---
+++ Knowledge_Work/Design_Pattern/protoType_Pattern/prototype_pattern.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+const int N = 4;
+
+// Prototype
+class cDevice 
+{
+public:
+   virtual cDevice* clone() const = 0;
+   virtual void store() const = 0;
+   virtual ~cDevice() { }
+};
+
+
+// Concrete prototypes : xmlDoc, plainDoc, spreadsheetDoc
+
+class cAppleIphone : public cDevice 
+{
+public:
+   cDevice*   clone() const { return new cAppleIphone; }
+   void store() const { cout << "Apple Iphone\n"; }
+};
+
+class cSamsungGalaxy : public cDevice
+{
+public:
+   cDevice* clone() const { return new cSamsungGalaxy; }
+   void store() const { cout << "Samsung Galaxy\n"; }
+};
+
+class cWindowsPhone : public cDevice
+{
+public:
+  cDevice* clone() const { return new cWindowsPhone; }
+   void store() const { cout << "Windows Phone\n"; }
+};
+
+// makeDevice() calls Concrete Portotype's clone() method 
+// inherited from Prototype
+class cDeviceManager {
+public:
+   static cDevice* createDevice( int choice );
+   ~cDeviceManager(){}
+
+private:
+   static cDevice* mDeviceTypes_[N];
+};
+
+cDevice* cDeviceManager::mDeviceTypes_[] = 
+{
+   0, new cSamsungGalaxy, new cAppleIphone, new cWindowsPhone
+};
+
+cDevice* cDeviceManager::createDevice( int choice ) 
+{
+   return mDeviceTypes_[choice]->clone();
+}
+
+// for_each op ()
+struct sDestruct
+{
+    void operator()(cDevice *a) const {
+     cout << "Object getting deleted " << endl;
+	delete a; 
+    }
+};
+
+// Client
+int main() {
+   vector<cDevice*> devices(N);
+   int choice;
+   cout << "quit(0), samsung Galaxy (1), AppleIphone (2), Windows Phone (3): \n";
+   while (true) {
+	  cout << "Type in your choice (0-3)\n";
+	  cin >> choice;
+      if (choice <= 0 || choice >= N)
+         break;
+      devices [choice] = cDeviceManager::createDevice ( choice );
+   }
+
+   for (int i = 1; i < devices.size(); ++i) 
+	   if(devices [i]) devices [i]->store();
+
+   struct sDestruct d;
+    // this calls Destruct::operator()
+    for_each(devices.begin(), devices.end(), d);
+
+   return 0;
+}
