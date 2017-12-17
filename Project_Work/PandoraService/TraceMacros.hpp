5ca0a79213d35b5687643e04ba2e07927e8cd940
Chatterjee, Monojit <Monojit.Chatterjee@harman.com>
2017-12-17 12:04:53 -0800

    Removing unnecessary changes

--- Project_Work/PandoraService/TraceMacros.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef TRACEMACROS_HPP_
-#define TRACEMACROS_HPP_
-
-#include "api/sys/tracesrv/pf/trace/src/private/HBTraceMacros.hpp"
-#include "api/sys/tracesrv/pf/trace/src/CTraceThread.hpp"
-#include "api/sys/tracesrv/pf/trace/src/CHBNullTracePersistence.hpp"
-#endif /*IOCTRACEMACROS_HPP_*/
