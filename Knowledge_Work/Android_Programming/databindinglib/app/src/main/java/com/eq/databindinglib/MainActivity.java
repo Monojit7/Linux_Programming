b970de45340d27faf69b2ca5553695371cb93091
Monojit Chatterjee <monty.joypur@gmail.com>
2020-05-08 02:24:27 +0530

    just checking

--- Knowledge_Work/Android_Programming/databindinglib/app/src/main/java/com/eq/databindinglib/MainActivity.java
+++ Knowledge_Work/Android_Programming/databindinglib/app/src/main/java/com/eq/databindinglib/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // checking if commit works 
       //  MyLayoutBinding binding = MyLayoutBinding
         myMap = new HashMap<>(2);
 
