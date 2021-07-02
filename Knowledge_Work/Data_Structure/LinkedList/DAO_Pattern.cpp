b9b5b3be60d2b31f625955dc9b2d68cf734e402f
Monojit Chatterjee <monty.joypur@gmail.com>
2021-07-02 16:29:47 +0530

    pushing design patterns

---
+++ Knowledge_Work/Data_Structure/LinkedList/DAO_Pattern.cpp
@@ -0,0 +1,154 @@
+/*
+
+Data access pattern is used to segregate low level API implementation with high level
+business service.
+When DataBase or any xml has been accessed ( any other storage container) to segregate the 
+database accessing implementation with high level business service this has been used.
+
+Data Access Object Interface : This interface class has all the methods which are used by
+                               business service
+Data Access Object Concrete class : This class has all the implementation logic which are mentioned
+                                    in the DAO interface class .All the Database related API's are
+                                    implemented here
+Model Object or Value Object : This class is simple C++ class which has get , set method and stores
+                               values accessed from DATA access object.
+                
+
+*/
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Student 
+{
+    public:
+    string name =  {};
+    int roll = 0;
+
+    Student ( string name , int roll)
+    {
+        this->name = name;
+        this->roll = roll;
+    }
+
+    int getRoll ()
+    {
+        return this->roll;
+    }
+
+    void setRoll ( int roll )
+    {
+        this->roll = roll;
+    }
+
+
+    string getName ()
+    {
+        return this->name;
+    }
+
+    void setName ( string name )
+    {
+        this->name = name;
+    }
+
+
+};
+
+class DAOInterface
+{
+
+public:
+    virtual list<Student*>& getAllStudents() = 0 ;
+    virtual Student* getStudent ( int roll ) = 0;
+    virtual void upDateStudent ( Student* student , string name ) = 0;
+    virtual void deleteStudent ( Student* student ) = 0;
+
+
+};
+
+class DAOImpl : public DAOInterface
+{
+  public :
+
+  list <Student*> students ;
+
+  DAOImpl ()
+  {
+      
+      Student* student1 = new Student ("Raj", 11 );
+      Student* student2 = new Student ("Rohit", 14 );
+
+      addStudent ( student1);
+      addStudent ( student2 );
+
+  }
+
+  list<Student*>& getAllStudents ()
+  {
+      return this->students;
+  }
+
+  Student* getStudent ( int roll )
+  {
+      for ( Student* st : getAllStudents() )
+      {
+          if ( st->getRoll() == roll )
+          {
+              return st;
+          }
+
+      }
+  }
+
+  void upDateStudent ( Student* student , string name )
+  {
+      cout << " update student list for student name " << student->name << " with " << name << endl;
+      auto it = find ( getAllStudents().begin(), getAllStudents().end(), student );
+      (*it)->setName (name);
+      cout << "updating  done "   << endl;
+  }
+
+  void deleteStudent ( Student* student )
+  {
+      cout << " deleting student " << " student name : " << student->name << endl;
+      this->getAllStudents().remove ( student);
+      delete student;
+      
+  } 
+
+  void addStudent ( Student* student )
+  {
+      cout << "  add student " << " student name : " << student->name << endl;
+      this->getAllStudents().push_back ( student );
+  }
+
+  void printDetails ( )
+  {
+      cout << " printDetails " << endl;
+      for ( Student* st : students )
+      {
+          cout << " name --> " << st->getName() << " roll --> " << st->getRoll() << endl;
+      }
+  }
+
+  
+} ;
+
+int main ()
+{
+    DAOImpl* dao = new DAOImpl;
+    Student* student = dao->getStudent ( 11 );
+    cout << " name : " << student->getName () << "  roll : " << student->getRoll() << endl;
+    dao->addStudent ( new Student ( "Rakesh", 15 ));
+    dao->printDetails();
+    dao->deleteStudent ( student );
+    dao->printDetails();
+    dao->upDateStudent ( dao->getStudent (14), "puja");
+    dao->printDetails ();
+
+    
+    dao->printDetails ();
+    return 0;
+}
---
+++ Knowledge_Work/Data_Structure/LinkedList/strategy.cpp
@@ -0,0 +1,105 @@
+/*
+
+In strategy pattern  , class behaviour or underlying algorithm can be changed at runtime .
+This is behaviour pattern.
+
+In strategy pattern, there are strategy object and context objects. if the strategy objects
+changes its algorithm context object also behaves accordingly.
+
+strategy object changes the behaviour of the context object
+
+*/
+
+/*
+
+In the below example strategy class has one interface doOperation which takes two number as arguments.
+
+startegy class has three subclasses operationAdd, operationSubstract, operationMultiply which implements
+
+doperation method.
+
+Context class has memeber of strategy class pointer which is assigned to proper concrete class ( operationAdd, operationSubstract, operationMultiply)
+in the constructor
+
+It has method executeStartegy which in turn invoke strategy class dooperation and corresponding strategy has been executed
+
+*/
+
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Strategy
+{
+    public:
+    virtual void doOperation ( int n1, int n2 ) = 0;
+};
+
+class operationAdd : public Strategy
+{
+    public:
+    void doOperation( int n1, int n2 )
+    {
+        cout << "the result is " << ( n1 + n2 ) << endl;
+    }
+};
+
+class operationMultiPly : public Strategy
+{
+    public:
+    void doOperation( int n1, int n2 )
+    {
+        cout << "the result is " << ( n1 * n2 ) << endl;
+    }
+};
+
+class operationSubstract : public Strategy
+{
+    public:
+    void doOperation( int n1, int n2 )
+    {
+        cout << "the result is " << ( n1 - n2 ) << endl;
+    }
+};
+
+class Context
+{
+    private: 
+    Strategy* mStrategy = nullptr;
+
+    public:
+    Context ( Strategy* mStrategy  )
+    {
+       this->mStrategy = mStrategy;
+    }
+
+    void executeStrategy ( int n1 , int n2 )
+    {
+        this->mStrategy->doOperation ( n1 , n2 );
+    }
+
+};
+
+
+int main ()
+{
+    int n1 = 11;
+    int n2 = 6;
+    Strategy* mStrategy = new operationAdd;
+    Context* mContext = new Context ( mStrategy );
+    mContext->executeStrategy ( n1, n2 );
+    mStrategy = new operationMultiPly;
+    Context* mContext1 = new Context ( mStrategy );
+    mContext1->executeStrategy ( n1, n2 );
+    mStrategy = new operationSubstract;
+    Context* mContext2 = new Context ( mStrategy );
+    mContext2->executeStrategy ( n1, n2 );
+}
+
+
+
+
+
+
